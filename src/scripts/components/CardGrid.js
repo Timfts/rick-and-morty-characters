fa66eb96d14e44095e3f339f096f3b20f1aba798
Italo Fontes <italo5123@hotmail.com>
2020-11-02 12:27:54 -0300

    :sparkles: added grid card space

--- src/scripts/components/CardGrid.js
+++ src/scripts/components/CardGrid.js
@@ -10,6 +10,7 @@ export default class CardGrid extends GlobalStoreManagerMixin(ShadowElement) {
   constructor() {
     super();
     this._renderCards = this._renderCards.bind(this);
+    this.gutterSize = '24px'
   }
 
   connectedCallback() {
@@ -19,12 +20,15 @@ export default class CardGrid extends GlobalStoreManagerMixin(ShadowElement) {
   }
 
   get _cardGridContainer() {
-    return this.querySelector(".card-grid");
+    return this.querySelector(".card-grid__container");
   }
 
   _renderCards({ currentState: newCharacters }) {
     const newCards = mapElements(newCharacters).render(
-      (character) => `<card-default>${character.name}</card-default>`
+      (character) =>
+        `<div class="card-grid__card-container">
+          <card-default>${character.name}</card-default>
+        </div>`
     );
 
     this._cardGridContainer.innerHTML = newCards;
@@ -34,11 +38,48 @@ export default class CardGrid extends GlobalStoreManagerMixin(ShadowElement) {
     return `
       <style> 
         .card-grid {
-          display: grid;
-          grid-template-columns: repeat(4, 1fr);
+          max-width: 1280px;
+          margin: 0 auto;
+          padding:5px 20px;
         }
+
+        .card-grid__container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+        }
+
+        .card-grid__card-container {
+          flex-shrink: 0;
+          flex-grow: 0;
+          width: calc(calc(100% - ${this.gutterSize}) / 2);
+          margin-bottom: ${this.gutterSize};
+          height: 200px;
+          background-color: blue;
+        }
+
+        @media(min-width: 600px) {
+          .card-grid__card-container {
+            width: calc(calc(100% - 2 * ${this.gutterSize}) / 3);
+          }
+        }
+
+        @media (min-width: 1280px) {
+          .card-grid {
+            padding: 5px 0;
+          }
+
+          .card-grid__card-container {
+            width: calc(calc(100% - 3 * ${this.gutterSize}) / 4);
+          }
+        }
+
+
       </style>
-      <div class="card-grid"></div>
+      <div class="card-grid">
+        <div class="card-grid__container">
+        </div>
+      </div>
     `;
   }
 }
